8fa3e35705a774b8534e7091d12f74792c60508e
Kosikan Nagananthakumaran <150826559+kosikan20@users.noreply.github.com>
2024-09-16 02:42:05 +0530

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
